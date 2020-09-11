d688595910f086993a10917eb87ee7296fa67023
chen <125559764>
2020-09-11 17:07:19 +0800

    test

---
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/config/DataSourceConfiguration.java
@@ -0,0 +1,33 @@
+package com.itheima.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@PropertySource("classpath:jdbc.properties")
+public class DataSourceConfiguration {
+    @Value("$(jdbc.driver)")
+    private String driver;
+    @Value("$(jdbc.url)")
+    private String url;
+    @Value("$(jdbc.userName)")
+    private String userName;
+    @Value("$(jdbc.password)")
+    private String password;
+
+    @Bean("dataSource")
+    public DataSource getDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(userName);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
---
+++ itheima_spring_ioc_anno/src/test/java/com/itheima/config/SpringConfiguration.java
@@ -0,0 +1,15 @@
+package com.itheima.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@ComponentScan("com.itheima")
+@Import(DataSourceConfiguration.class)
+public class SpringConfiguration {
+
+}
